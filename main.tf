55ac6f3d66a8304de5dc47dc6442f270e4787648
Alex Lubneuski <lubneuskia@chop.edu>
2024-09-25 11:13:49 -0400

    :tada: Added SecretsManager VPC Endpoint

--- main.tf
+++ main.tf
@@ -233,6 +233,26 @@ resource "aws_vpc_endpoint" "ssm" {
   )
 }
 
+resource "aws_vpc_endpoint" "ssm" {
+  vpc_id            = aws_vpc.default.id
+  service_name      = "com.amazonaws.${var.region}.secretsmanager"
+  vpc_endpoint_type = "Interface"
+
+  security_group_ids = [
+    aws_security_group.vpc_endpoint.id
+  ]
+
+  subnet_ids          = aws_subnet.private.*.id
+  private_dns_enabled = true
+
+  tags = merge(
+    {
+      Name = "${var.name}-endpointSsm"
+    },
+    var.tags
+  )
+}
+
 resource "aws_vpc_endpoint" "ssmmessages" {
   vpc_id            = aws_vpc.default.id
   service_name      = "com.amazonaws.${var.region}.ssmmessages"
