b9b6d7792704abc1fc6c06b614964ba103f494b3
Alex Lubneuski <lubneuskia@chop.edu>
2024-09-25 12:12:16 -0400

    :wrench: Service type Gateway is not available for SecretsManager vpc endpoint

--- main.tf
+++ main.tf
@@ -236,11 +236,14 @@ resource "aws_vpc_endpoint" "ssm" {
 resource "aws_vpc_endpoint" "secretsmanager" {
   vpc_id            = aws_vpc.default.id
   service_name      = "com.amazonaws.${var.region}.secretsmanager"
+  vpc_endpoint_type = "Interface"
 
-  route_table_ids = flatten([
-    aws_route_table.public.id,
-    aws_route_table.private.*.id
-  ])
+  security_group_ids = [
+    aws_security_group.vpc_endpoint.id
+  ]
+
+  subnet_ids          = aws_subnet.private.*.id
+  private_dns_enabled = true
 
   tags = merge(
     {
