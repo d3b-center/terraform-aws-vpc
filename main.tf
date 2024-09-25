6aefe8760bede07d4cfff0a00a2142167943aa64
Alex Lubneuski <lubneuskia@chop.edu>
2024-09-25 14:22:49 -0400

    :wrench: Added CloudWatch Endpoint

--- main.tf
+++ main.tf
@@ -253,6 +253,26 @@ resource "aws_vpc_endpoint" "secretsmanager" {
   )
 }
 
+resource "aws_vpc_endpoint" "cloudwatch" {
+  vpc_id            = aws_vpc.default.id
+  service_name      = "com.amazonaws.${var.region}.cloudwatch"
+  vpc_endpoint_type = "Interface"
+
+  security_group_ids = [
+    aws_security_group.vpc_endpoint.id
+  ]
+
+  subnet_ids          = aws_subnet.private.*.id
+  private_dns_enabled = true
+
+  tags = merge(
+    {
+      Name = "${var.name}-endpointCloudWatch"
+    },
+    var.tags
+  )
+}
+
 resource "aws_vpc_endpoint" "ecr_api" {
   vpc_id            = aws_vpc.default.id
   service_name      = "com.amazonaws.${var.region}.ecr.api"
