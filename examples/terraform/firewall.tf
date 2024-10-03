7ca1792186da5dcfd9628e0b954aa581ff9deeda
devbyaccident <blackdenc@chop.edu>
2024-10-03 14:22:14 -0400

    Removing IPv6

--- examples/terraform/firewall.tf
+++ examples/terraform/firewall.tf
@@ -7,7 +7,6 @@ resource "aws_security_group_rule" "bastion_https_egress" {
   to_port          = 443
   protocol         = "tcp"
   cidr_blocks      = ["0.0.0.0/0"]
-  ipv6_cidr_blocks = ["::/0"]
 
   security_group_id = module.vpc.bastion_security_group_id
 }
